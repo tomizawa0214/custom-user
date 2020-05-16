2d43abf2a82148663388fe7a443349efde3bcb02
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-05-16 21:56:26 +0900

    プロフィール画面完成

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
 from django.views import View
+from accounts.models import CustomUser
 from django.shortcuts import render, redirect
 
 class ProfileView(View):
   def get(self, request, *args, **kwargs):
-    return render(request, 'accounts/profile.html')
+    user_data = CustomUser.objects.get(id=request.user.id)
+
+    return render(request, 'accounts/profile.html', {
+      'user_data': user_data,
+    })
