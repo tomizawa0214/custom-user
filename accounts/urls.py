50943ffd061048ea612d76ea5704563586aa1daa
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-05-17 13:42:04 +0900

    ログアウト画面追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from accounts import views
 
 urlpatterns = [
   path('login/', views.LoginView.as_view(), name='account_login'),
+  path('logout/', views.LogoutView.as_view(), name='account_logout'),
   path('profile/', views.ProfileView.as_view(), name='profile'),
   path('profile/edit/', views.ProfileEditView.as_view(), name='profile_edit'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,14 @@ from accounts.forms import ProfileForm
 from django.shortcuts import render, redirect
 from allauth.account import views
 
+class LogoutView(views.LogoutView):
+  template_name = 'accounts/logout.html'
+
+  def post(self, *args, **kwargs):
+    if self.request.user.is_authenticated:
+      self.logout()
+    return redirect('/')
+
 class LoginView(views.LoginView):
   template_name = 'accounts/login.html'
 
