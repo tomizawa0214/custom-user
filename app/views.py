88d71bc7cb3a8a43c1401b84dd2307825757ccc6
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-15 22:12:02 +0900

    インデント整理

--- app/views.py
+++ app/views.py
@@ -2,5 +2,5 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(LoginRequiredMixin, TemplateView):
-  template_name = "app/index.html"
-  login_url = '/accounts/login/'
+    template_name = "app/index.html"
+    login_url = '/accounts/login/'
